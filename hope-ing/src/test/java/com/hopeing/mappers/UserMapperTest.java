d3eac6b07b6353a1d8656bfd2e0c508c11dac17a
Ji-0 <xaonoax@gmail.com>
2023-07-03 22:24:44 +0900

    회원가입 구현 테스트

--- hope-ing/src/test/java/com/hopeing/mappers/UserMapperTest.java
+++ hope-ing/src/test/java/com/hopeing/mappers/UserMapperTest.java
@@ -16,21 +16,6 @@ public class UserMapperTest {
 	@Autowired
 	private UserMapper userMapper;
 	
-	// 아이디 중복 테스트 메서드
-//	@Test
-	public void joinCheckDuplicateIdTest() {
-		String user_id = "test1"; // 중복되는 아이디로 테스트
-		
-		UserVO user = userMapper.joinCheckDuplicateId(user_id);
-
-		if (user != null) {
-			log.info("중복된 아이디입니다.");
-		}
-		else {
-			log.info("중복된 아이디가 아닙니다.");
-		}
-	}
-	
 	// 회원가입 테스트 메서드
 //	@Test
 	public void joinTest() {
