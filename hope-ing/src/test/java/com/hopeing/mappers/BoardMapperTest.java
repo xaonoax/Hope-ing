c741f751c00a908a65569707aa644dd30777498f
Ji-0 <xaonoax@gmail.com>
2023-07-14 16:45:08 +0900

    커뮤니티 게시글 목록, 등록 테스트

--- hope-ing/src/test/java/com/hopeing/mappers/BoardMapperTest.java
+++ hope-ing/src/test/java/com/hopeing/mappers/BoardMapperTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.hopeing.beans.vo.BoardVO;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
@@ -13,6 +15,18 @@ import lombok.extern.slf4j.Slf4j;
 public class BoardMapperTest {
 	@Autowired
 	private BoardMapper boardMapper;
+	
+	// 글 등록 테스트
+//	@Test
+	public void insert() {
+		BoardVO board = new BoardVO();
+		
+		board.setBoard_title("title");
+		board.setBoard_content("content");
+		board.setBoard_writer_nickname("nickname");
+		
+		// boardMapper.register(board);
+	}
 	
 	// 게시판 목록 테스트
 //	@Test
