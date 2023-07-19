77cd20835737f69c9487b0b3ef3680c5726610be
Ji-0 <xaonoax@gmail.com>
2023-07-19 22:19:55 +0900

    댓글 기능 구현(수정, 삭제)

--- hope-ing/src/main/java/com/hopeing/controller/ReplyController.java
+++ hope-ing/src/main/java/com/hopeing/controller/ReplyController.java
@@ -1,14 +1,17 @@
 package com.hopeing.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hopeing.beans.vo.ReplyVO;
@@ -27,16 +30,58 @@ import lombok.extern.slf4j.Slf4j;
 public class ReplyController {
 	private final ReplyService replyService;
 	
+	// 댓글 삭제
+	@DeleteMapping(value="{reply_no}", produces="text/plain; charset=utf-8")
+	public String delete(@PathVariable("reply_no") Long reply_no) {
+		
+		return replyService.delete(reply_no) == 1 ? "댓글을 삭제하였습니다." : "댓글 삭제에 실패하였습니다.";
+	}
+	
+	// 댓글 수정
+	@RequestMapping(method= {RequestMethod.PATCH},
+			value= {"/{reply_no}", "/{reply_no}/{reply_writer_id}"},
+			consumes="application/json",
+			produces="text/plain; charset=utf-8")
+	public ResponseEntity<String> update(
+			@RequestBody ReplyVO replyVO,
+			@PathVariable(value = "reply_writer_id", required = false) String reply_writer_id,
+			@PathVariable("reply_no") Long reply_no,
+			HttpServletRequest request)
+	throws UnsupportedEncodingException {
+		HttpSession session = request.getSession();
+	    UserVO user = (UserVO) session.getAttribute("user");
+	    
+	    replyVO.setReply_writer_id(user.getUser_id());
+		replyVO.setReply_no(reply_no);
+		
+		int replyCount = 0;
+		log.info("modify ------------------------------------> " + reply_no);
+		log.info("modify : " + replyVO);
+		
+		
+		if(replyVO.getReply_writer_id() == null) {
+			replyVO.setReply_writer_id(Optional.ofNullable(reply_writer_id).orElse("anonymous"));
+		}
+		replyCount = replyService.update(replyVO);
+		
+		if(replyCount == 1) {
+			return new ResponseEntity<>(new String("댓글을 수정하였습니다.".getBytes(), "UTF-8"), HttpStatus.OK);
+		}
+		else {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 	// 각 게시글별 댓글 조회
 	@GetMapping("{reply_no}")
-	public ReplyVO get(@PathVariable("reply_no") Long reply_no) {
+	public ReplyVO read(@PathVariable("reply_no") Long reply_no) {
 		
 		return replyService.read(reply_no);
 	}
 	
 	// 댓글 등록
 	@PostMapping(value="/new", consumes="application/json", produces="text/plain; charset=utf-8")
-	public ResponseEntity<String> create(@RequestBody ReplyVO reply, HttpServletRequest request)
+	public ResponseEntity<String> register(@RequestBody ReplyVO reply, HttpServletRequest request)
 			throws UnsupportedEncodingException {
 		HttpSession session = request.getSession();
 		UserVO user = (UserVO) session.getAttribute("user");
