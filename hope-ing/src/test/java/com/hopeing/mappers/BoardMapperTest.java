1c02ebdd60ffff5560b82e2f5e3a7d71be9dc48f
Ji-0 <xaonoax@gmail.com>
2023-07-20 21:14:37 +0900

    게시글 목록 페이징 테스트

--- hope-ing/src/test/java/com/hopeing/mappers/BoardMapperTest.java
+++ hope-ing/src/test/java/com/hopeing/mappers/BoardMapperTest.java
@@ -1,11 +1,15 @@
 package com.hopeing.mappers;
 
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.hopeing.beans.vo.BoardVO;
+import com.hopeing.beans.vo.Criteria;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,9 +20,21 @@ public class BoardMapperTest {
 	@Autowired
 	private BoardMapper boardMapper;
 	
+	// 게시글 목록 페이징
+	@Test
+	public void testGetListWithPaging() {
+		Criteria cri = new Criteria();
+		
+		cri.setAmount(1);
+		cri.setPageNum(0);
+		
+		List<BoardVO> list = boardMapper.getListWithPaging(cri);
+		list.forEach(board -> log.info(board.getBoard_no() + ""));
+	}
+	
 	// 글 등록 테스트
 //	@Test
-	public void insert() {
+	public void register() {
 		BoardVO board = new BoardVO();
 		
 		board.setBoard_title("title");
