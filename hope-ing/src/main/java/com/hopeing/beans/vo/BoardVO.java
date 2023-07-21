6b511741c129f814b2e3c02660b8d39f8b95b3f6
Ji-0 <xaonoax@gmail.com>
2023-07-22 01:22:05 +0900

    조회수, 좋아요 수 타입 변경

--- hope-ing/src/main/java/com/hopeing/beans/vo/BoardVO.java
+++ hope-ing/src/main/java/com/hopeing/beans/vo/BoardVO.java
@@ -13,8 +13,8 @@ public class BoardVO {
 	private String board_title;				// 게시글 제목
 	private String board_content;			// 게시글 내용
 	private String board_writer_id;			// 작성자 아이디
-	private String board_views;				// 게시글 조회수
-	private String board_like_count;		// 게시글 좋아요 수
+	private Long board_views;				// 게시글 조회수
+	private Long board_like_count;		// 게시글 좋아요 수
 	private Date board_regdate;				// 게시글 작성일
 	private Date board_updatedate;			// 게시글 수정일
 }
