114eeb56ac1882696939427b49dca9db3ce34ff3
Ji-0 <xaonoax@gmail.com>
2023-07-25 23:36:48 +0900

    좋아요 구현을 위해 UserVO, BoardVO 타입의 필드 추가

--- hope-ing/src/main/java/com/hopeing/beans/vo/LikeVO.java
+++ hope-ing/src/main/java/com/hopeing/beans/vo/LikeVO.java
@@ -7,6 +7,9 @@ import lombok.Data;
 @Data
 @Component
 public class LikeVO {
+	private UserVO user;  // UserVO 타입의 필드 추가
+	private BoardVO board;  // BoardVO 타입의 필드 추가
+	
 	private Long like_no;		// 좋아요 번호
 	private Long like_bno;		// 좋아요한 게시글 번호
 	private String like_id;		// 좋아요한 사용자
