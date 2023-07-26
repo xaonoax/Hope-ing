3fa6e2bafd74adbde707cfd7f021894a83b98c8a
Ji-0 <xaonoax@gmail.com>
2023-07-26 22:56:06 +0900

    파일 업로드 구현을 위한 파일 이름, 경로 DB필드 추가

--- hope-ing/src/main/java/com/hopeing/beans/vo/BoardVO.java
+++ hope-ing/src/main/java/com/hopeing/beans/vo/BoardVO.java
@@ -14,7 +14,9 @@ public class BoardVO {
 	private String board_content;			// 게시글 내용
 	private String board_writer_id;			// 작성자 아이디
 	private Long board_views;				// 게시글 조회수
-	private Long board_like_count;		// 게시글 좋아요 수
+	private Long board_like_count;			// 게시글 좋아요 수
 	private Date board_regdate;				// 게시글 작성일
 	private Date board_updatedate;			// 게시글 수정일
+	private String board_file_name;			// 파일 이름
+	private String board_file_path;			// 파일 경로
 }
